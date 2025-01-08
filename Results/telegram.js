9e8b445c817af707c991c9b2354c697aadf8af9e
Somdev Behera <64832169+somdev-hub@users.noreply.github.com>
2025-01-08 14:23:49 +0530

    updated Telegram bot webhook URL to production environment

--- Results/telegram.js
+++ Results/telegram.js
@@ -5,7 +5,7 @@ dotenv.config();
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_KEY);
 
 bot.setWebHook(
-  `https://fetch-marks-1.onrender.com/${process.env.TELEGRAM_BOT_KEY}`
+  `https://fetch-marks-production.up.railway.app/${process.env.TELEGRAM_BOT_KEY}`
 );
 
 // const bot = new TelegramBot(process.env.TELEGRAM_BOT_KEY, { polling: true });
